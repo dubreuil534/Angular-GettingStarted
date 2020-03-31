8ea0037351de87e6cf825f7297a0f34b88dee118
Dominic Dubreuil <dubreuil534@hotmail.com>
2020-03-31 11:56:38 -0400

    create template component

---
+++ APM-Start/src/app/products/product-list-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
+})
+
+
+
+
+export class ProductListComponent{
+
+
+
+}
